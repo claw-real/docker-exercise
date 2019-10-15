f547ce8f362bb26d8e96950d251fbb95e4bbf211
Chris Law <chris.law@mediakind.com>
2019-10-15 21:26:23 +0100

    Multiple run script

--- run_docker.sh
+++ run_docker.sh
@@ -3,6 +3,20 @@
 docker run -d \
   --rm \
   --name ws1 \
-  -v $PWD/html/ws1.html:/var/www/html/index.html \
+  -v "$PWD"/html/ws1.html:/var/www/html/index.html \
   -p 1180:80 \
+  claw/test-nginx
+
+docker run -d \
+  --rm \
+  --name ws2 \
+  -v "$PWD"/html/ws2.html:/var/www/html/index.html \
+  -p 1280:80 \
+  claw/test-nginx
+
+docker run -d \
+  --rm \
+  --name ws3 \
+  -v "$PWD"/html/ws3.html:/var/www/html/index.html \
+  -p 1380:80 \
   claw/test-nginx
